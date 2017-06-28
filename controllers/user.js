24e7ba17b8665be6406058dbab7c28e4ebadf2e9
Edwin Castillo <eocc28@hotmail.com>
2017-06-27 21:55:21 -0500

    Validación del email y la contraseña haciendo uso del Schema comparePassword

--- controllers/user.js
+++ controllers/user.js
@@ -3,6 +3,7 @@
 const mongoose = require('mongoose')
 const User = require('../models/user')
 const service = require('../services')
+const bcrypt = require('bcrypt-nodejs')
 
 function signUp (req, res) {
 	const user = new User({
@@ -18,12 +19,15 @@ function signUp (req, res) {
 }
 
 function signIn (req, res) {
-	User.find( { email: req.query.email }, (err, user) => {
-		if (err) return res.status(500).send( { message: err } )
-		if (!user) return res.status(404).send( { message: 'Este usuario no existe' } )
+	User.findOne( { email: req.query.email }, (err, user) => {
 
-		
-		res.status(200).send( { message: 'Te has logueado correctamente', token: service.createToken(user), user } )
+		user.comparePassword(req.query.password, (err, isMatch) => {
+			if (err) throw err
+			if (!isMatch) return res.status(403).send( { message: 'Las credenciales no coinciden' } )
+
+			req.user = user;		
+			return res.status(200).send( { message: 'Te has logueado correctamente', token: service.createToken(user), user } )
+		})
 	})
 }
 
@@ -32,7 +36,7 @@ function getUsers(req, res) {
 		if (err) return res.status(500).send( { message: `Error al realizar la petición` } )
 		if (!users) return res.status(404).send( { message: `No existen productos en la DB` } )
 
-		res.send(200, { users })
+		res.status(200).send({ users })
 	})
 }
 
--- models/user.js
+++ models/user.js
@@ -36,4 +36,11 @@ UserSchema.methods.gravatar = function () {
 	return 'https://gravatar.com/avatar/${md5}?s=200&d=retro'
 }
 
+UserSchema.methods.comparePassword = function (candidatePassword, cb) {
+	bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+}
+
 module.exports = mongoose.model('User', UserSchema)
